5f313108686c0b6b6eaec2e0ce308275680758e3
ravi8470 <rvkmr0851@gmail.com>
2020-05-18 02:45:56 +0530

    changed readme and homepage description

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { RedditService } from '../reddit.service';
 
 @Component({
-    selector: 'app-example',
-    templateUrl: './example.component.html',
-    styleUrls: ['./example.component.css']
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css']
 })
 export class ExampleComponent implements OnInit {
 
-    constructor(private service: RedditService) { }
-    data1: string;
-    ngOnInit() {
-        this.service.currentMessage.subscribe((data: any) => { this.data1 = data.first_name })
-    }
+  data1: string;
+
+  constructor(private service: RedditService) { }
+
+
+  ngOnInit() {
+    // this.service.currentMessage.subscribe((data: any) => { this.data1 = data.first_name })
+  }
 
 }
--- src/app/reddit.service.ts
+++ src/app/reddit.service.ts
@@ -4,56 +4,57 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class RedditService {
 
-    private messageSource = new BehaviorSubject('blank');
-    // private messageSource = new Subject();
-    currentMessage = this.messageSource.asObservable();
+  private messageSource = new BehaviorSubject('blank');
+  // private messageSource = new Subject();
+  currentMessage = this.messageSource.asObservable();
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    getPosts(): Observable<any> {
-        return this.http.get('https://www.reddit.com/r/programming.json').pipe(
-            pluck('data', 'children')
-        )
-    }
+  getPosts(): Observable<any> {
+    return this.http.get('https://www.reddit.com/r/programming.json').pipe(
+      pluck('data', 'children')
+    )
+  }
 
-    getNewsIds(): Observable<any> {
-        // GET NEWS IDS AS ARRAY AND THEN SLICE IT AND RETURN IT AS OBSERVABLE USING RXJS MAP CODE
-        // return this.http.get('https://hacker-news.firebaseio.com/v0/topstories.json').pipe(map((x: Array<any>) => {
-        //     console.log(x);
-        //     return x.slice(0, 10)
-        // }))
-        return this.http.get('https://hacker-news.firebaseio.com/v0/topstories.json')
-    }
+  getNewsIds(): Observable<any> {
+    // GET NEWS IDS AS ARRAY AND THEN SLICE IT AND RETURN IT AS OBSERVABLE USING RXJS MAP CODE
+    // return this.http.get('https://hacker-news.firebaseio.com/v0/topstories.json').pipe(map((x: Array<any>) => {
+    //     console.log(x);
+    //     return x.slice(0, 10)
+    // }))
+    return this.http.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+  }
 
-    getMovies(term, pageNum): Observable<any> {
-        return this.http.get(`http://www.omdbapi.com/?apikey=dabff1e2&s=${term}&page=${pageNum}`)
-    }
+  getMovies(term, pageNum): Observable<any> {
+    return this.http.get(`http://www.omdbapi.com/?apikey=dabff1e2&s=${term}&page=${pageNum}`)
+  }
 
-    getNews(idArr: Array<any>): Observable<any> {
-        return forkJoin(idArr.map(id => this.http.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)))
-    }
+  getNews(idArr: Array<any>): Observable<any> {
+    return forkJoin(idArr.map(id => this.http.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)))
+  }
 
-    getCommentsRecursive(id) {
-        this.http.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).subscribe((y: any) => {
+  getCommentsRecursive(id) {
+    this.http.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).subscribe((y: any) => {
 
-            if (y.type == "story") {
-                console.log('title: >>', y.title);
-            }
-            if (y.type == "comment") {
-                console.log('Comment: >>', y.text);
-            }
-            if (y.kids) {
-                this.getCommentsRecursive(y.kids[0])
-            }
-        })
-    }
-    testCall() {
-        this.http.get('https://reqres.in/api/users/2').subscribe((data: any) => {
-            this.messageSource.next(data.data);
-        });
-    }
+      if (y.type == "story") {
+        console.log('title: >>', y.title);
+      }
+      if (y.type == "comment") {
+        console.log('Comment: >>', y.text);
+      }
+      if (y.kids) {
+        this.getCommentsRecursive(y.kids[0])
+      }
+    })
+  }
+
+  testCall() {
+    this.http.get('https://reqres.in/api/users/2').subscribe((data: any) => {
+      this.messageSource.next(data.data);
+    });
+  }
 }
