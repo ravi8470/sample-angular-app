1b59609187c1907a32ba156f96f1f7e9e582f2fb
ravi8470 <rvkmr0851@gmail.com>
2020-05-18 02:56:02 +0530

    removed example2 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,49 +23,47 @@ import { MovieGridComponent } from './movie-grid/movie-grid.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { MatInputModule } from '@angular/material/input';
-import { Example2Component } from './example2/example2.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        RedditBrowserComponent,
-        NewsBrowserComponent,
-        FlexboxComponent,
-        ExampleComponent,
-        MovieGridComponent,
-        Example2Component,
-        LoginComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatCardModule,
-        MatDividerModule,
-        MatListModule,
-        MatToolbarModule,
-        FlexLayoutModule.withConfig({
-            useColumnBasisZero: false,
-            printWithBreakpoints: ['md', 'lt-lg', 'lt-xl', 'gt-sm', 'gt-xs']
-        }),
-        ObserversModule,
-        PlatformModule,
-        MatButtonModule,
-        NgxSpinnerModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatGridListModule,
-        NgxPaginationModule,
-        MatInputModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatProgressSpinnerModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    RedditBrowserComponent,
+    NewsBrowserComponent,
+    FlexboxComponent,
+    ExampleComponent,
+    MovieGridComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatToolbarModule,
+    FlexLayoutModule.withConfig({
+      useColumnBasisZero: false,
+      printWithBreakpoints: ['md', 'lt-lg', 'lt-xl', 'gt-sm', 'gt-xs']
+    }),
+    ObserversModule,
+    PlatformModule,
+    MatButtonModule,
+    NgxSpinnerModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatGridListModule,
+    NgxPaginationModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
