5ddf9fc38255b191c28012521a838a883c0b3718
ravi8470 <rvkmr0851@gmail.com>
2020-05-18 15:32:48 +0530

    formatted this file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { RedditService } from './reddit.service';
-import { Router, NavigationStart } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
-// import { MatToolbar } from "@angular/material/toolbar";
-// import { MatButton } from "@angular/material/button";
-// import { MatIcon } from "@angular/material/icon";
+import { RedditService } from './reddit.service';
+
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    constructor(private redditService: RedditService, private router: Router, private authService: AuthService) { }
-    isLoggedIn: Boolean;
-    ngOnInit() {
-        // this.redditService.getCommentsRecursive(20890682);
-        this.redditService.testCall();
-        this.isLoggedIn = !!this.authService.currentUserValue ? true : false;
-        this.authService.currentUser.subscribe(data => { console.log(data); this.isLoggedIn = data ? true : false });
-        console.log('this.islogg', this.isLoggedIn);
-    }
-    logout() {
-        this.authService.logout();
-    }
+
+  isLoggedIn: Boolean;
+
+  constructor(
+    private redditService: RedditService,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.redditService.testCall();
+    this.isLoggedIn = !!this.authService.currentUserValue ? true : false;
+    this.authService.currentUser.subscribe(data => { console.log(data); this.isLoggedIn = data ? true : false });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
 
